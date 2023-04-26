7ba0fa48b454b109831124ba5966894374a96afb
Madness75 <clementriou2a@gmail.com>
2023-04-26 17:48:53 +0200

    Changement agenda

--- Agenda.cpp
+++ Agenda.cpp
@@ -9,5 +9,14 @@
 Agenda::Agenda(const std::map<Date, std::map<Heure, std::string>> &rendezVous) : rendezVous(rendezVous) {}
 
 Agenda Agenda::ajouterRendezVous(std::map<Date, std::map<Heure, std::string>> rendezVousAjoute) {
-
+    for (auto& rdv : rendezVousAjoute) {
+        Date date = rdv.first;
+        std::map<Heure, std::string> rdvHeure = rdv.second;
+        for (auto& rdvH : rdvHeure) {
+            Heure heure = rdvH.first;
+            std::string note = rdvH.second;
+            rendezVous[date][heure] = note;
+        }
+    }
+    return *this;
 }
